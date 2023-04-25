303ea9de907255a678e770b078d1e317e413d3af
ammarsiddiqui123 <127627240+ammarsiddiqui123@users.noreply.github.com>
2023-04-25 17:14:56 +0500

    Update Program.cs

--- dotnet-deploy/Program.cs
+++ dotnet-deploy/Program.cs
@@ -1,8 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-
+builder.Services.AddRazorPages() 
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
